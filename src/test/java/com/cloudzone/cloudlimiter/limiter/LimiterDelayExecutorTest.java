5c5d72ce8b833e47b58bcc937c5c17815c386f48
tantexian <tantexian@qq.com>
2017-04-14 14:36:20 +0800

    增加执行频率的测试用例

--- src/test/java/com/cloudzone/cloudlimiter/limiter/LimiterDelayExecutorTest.java
+++ src/test/java/com/cloudzone/cloudlimiter/limiter/LimiterDelayExecutorTest.java
@@ -1,6 +1,8 @@
 package com.cloudzone.cloudlimiter.limiter;
 
 import com.cloudzone.cloudlimiter.factory.CloudFactory;
+import com.cloudzone.cloudlimiter.meter.CloudMeter;
+import com.cloudzone.cloudlimiter.meter.MeterListenerIpml;
 import org.junit.Test;
 
 import java.util.Date;
@@ -36,5 +38,33 @@ public class LimiterDelayExecutorTest {
             realTimeLimiter.acquire();
             System.out.println(new Date());
         }
+    }
+
+    @Test
+    public void test3() {
+        final RealTimeLimiter realTimeLimiter = CloudFactory.createRealTimeLimiter(LimiterDelayConstants.ONCE_PER_MINUTE);
+        CloudMeter cloudMeter = CloudFactory.createCloudMeter();
+        cloudMeter.registerListener(new MeterListenerIpml());
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // CloudTicker.sleepSeconds(10);
+                // 此处启动线程设置1000必须等到一分钟后才生效（因为初始化为每分钟执行一次，不允许中断）
+                System.out.println("---1000");
+                realTimeLimiter.setRate(1000);
+            }
+        });
+
+        thread.start();
+        int i = 0;
+        while (true) {
+            i++;
+            realTimeLimiter.acquire();
+            cloudMeter.request();
+            // System.out.println(new Date());
+        }
+
+
     }
 }
